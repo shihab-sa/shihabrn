b8092f66640de2eb58ae27cf439bfe1433506751
shihabahmed <shihabbee@gmail.com>
2024-06-21 12:39:18 +0600

    next ui and desi ui combine this projects

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,16 @@
 // tailwind.config.js
+const { nextui } = require("@nextui-org/react");
 module.exports = {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+  ],
   // theme: {
   //   extend: {},
   // },
   daisyui: {
     themes: ["light", "black", "cupcake", "night", "nord", "synthwave"],
   },
-  plugins: [require("daisyui")],
+  plugins: [require("daisyui"), nextui()],
 };
